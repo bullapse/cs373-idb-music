5f96184ab2c54820b2312a96c77633a51d30f6a7
Spencer Bull <spencerbull2554@gmail.com>
2017-03-21 21:51:24 -0500

    update appengine_config to import vendor

--- appengine_config.py
+++ appengine_config.py
@@ -1,4 +1,6 @@
-from google.appengine.ext import vendor
-
-# Add any libraries installed in the "lib" folder.
+"""`appengine_config` gets loaded when starting a new application instance."""
+import vendor
+# insert `lib` as a site directory so our `main` module can load
+# third-party libraries, and override built-ins with newer
+# versions.
 vendor.add('lib')
