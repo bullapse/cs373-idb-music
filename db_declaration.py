4ac749613ec28f8c3198d8864e1b9c3b1413324e
Sam Nguyen <tsnguyen@utexas.edu>
2017-03-21 21:30:50 -0500

    changed the attributes in the declaration

--- db_declaration.py
+++ db_declaration.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String, Date, BigInteger
+from sqlalchemy import Column, ForeignKey, Integer, String, Date, BigInteger, Boolean
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
