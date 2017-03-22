58847ac8318abf3bf1cdc859d3b4060b9a96fb6e
Sam Nguyen <tsnguyen@utexas.edu>
2017-03-21 20:53:24 -0500

    added script to insert into db

--- db_declaration.py
+++ db_declaration.py
@@ -11,28 +11,32 @@ class Artist(Base):
     __tablename__ = 'artist'
     id = Column(Integer, primary_key = True)
     name = Column(String(64), nullable = False)
+    spotify_id = Column(String(32), nullable=False)
+    image_url = Column(String(128))
+    followers = Column(Integer)
     popularity = Column(Integer, nullable = False)
     genre = Column(String(32))
-    album_count = Column(Integer, nullable = False)
 
 class Album(Base):
     __tablename__ = 'album'
     id = Column(Integer, primary_key = True)
     name = Column(String(64), nullable = False)
+    spotify_id = Column(String(32), nullable=False)
+    image_url = Column(String(128))
     release_date = Column(Date, nullable = False)
     number_of_tracks = Column(Integer, nullable = False)
     artist_id = Column(Integer, ForeignKey('artist.id'), nullable = False)
     popularity = Column(Integer, nullable = False)
-    runtime = Column(BigInteger, nullable = False)
 
 class Track(Base):
     __tablename__ = 'track'
     id = Column(Integer, primary_key = True)
     name = Column(String(64), nullable = False)
+    spotify_id = Column(String(32), nullable=False)
     artist_id = Column(Integer, ForeignKey('artist.id'), nullable = False)
     album_id = Column(Integer, ForeignKey('album.id'), nullable = False)
+    explicit = Column(Boolean)
     runtime = Column(Integer, nullable = False)
-    release_date = Column(Date, nullable = False)
     popularity = Column(Integer, nullable = False)
     preview_url = Column(String(128), nullable = False)
 
