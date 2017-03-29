200397c6f72b4829a3c4cad09ad89536d3569d7d
Spencer Bull <spencerbull2554@gmail.com>
2017-03-29 17:30:45 -0500

    start on side project of authorizing users with spotify and getting a users playlist.

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import requests
 app = Flask(__name__)
+
+
+CLIENT_ID = "78237eb54be441c7bafdf02459e9d5ad"
+CLIENT_SECRET = "3cde3d481c8b432ba6800e80412722a9"
 
 
 @app.route("/")
@@ -27,9 +32,40 @@ def albums():
 def tracks():
     return render_template('tracks.html')
 
+
 @app.route("/about.html")
 def about():
     return render_template('about.html')
+
+
+@app.route("/spotifycallback", methods=['GET'])
+def spotifycallback():
+    code = request.args.get('code')
+    state = request.args.get('state')
+    error = requests.args.get('state')
+    if error is not None:
+        console.log("ERROR: " + error)
+    else:
+        body = {
+            "grant_type": "authorization_code",
+            "code": code,
+            "redirect_uri": "https%3A%2F%2Fnotspotify.me%2Fspotfiycallback"
+        }
+        payload = {
+            "client_id": CLIENT_ID,
+            "client_secret": CLIENT_SECRET
+        }
+        res = requests.post('https://accounts.spotify.com/api/token', data=body, params=payload)
+
+        # Create a user with the following creds
+        access_token = res['access_token']
+        token_type = res['token_type']
+        scope = res['scope']
+        expires_in = res['expires_in']
+        refresh_token = res['refresh_token']
+        # Stoped at step 6 https://developer.spotify.com/web-api/authorization-guide/
+    return render_template('index.html')
+
 
 
 if __name__ == "__main__":
--- getSpotify.py
+++ getSpotify.py
@@ -10,13 +10,35 @@
 # imports
 # -------
 
-import sys
+from requests import get
 
-import requests
+CLIENT_ID = "78237eb54be441c7bafdf02459e9d5ad"
+CLIENT_SECRET = "3cde3d481c8b432ba6800e80412722a9"
 
-ARTISTS = ["4LZ4De2MoO3lP6QaNCfvcu", "62Jfwxon19ZOT9eSL6bvtY", "2cFrymmkijnjDg9SS92EPM"]
-ALBUMS = ["6S0sbdQmuF3IhNRcMkuQK3", "4w0aS3VhSU7QHEN4zfpvHv", "41B7cBcRZDSE62bo0eoBTW"]
-TRACKS = ["1OcHQQ7A239YbKqKBYw2yw", "2BvI93upqv44QI4hVTvAC3", "53wXk7sMOnvkdAcEpGzu8W"]
+ARTISTS = ["4LZ4De2MoO3lP6QaNCfvcu",
+           "62Jfwxon19ZOT9eSL6bvtY",
+           "2cFrymmkijnjDg9SS92EPM"]
+ALBUMS = ["6S0sbdQmuF3IhNRcMkuQK3",
+          "4w0aS3VhSU7QHEN4zfpvHv",
+          "41B7cBcRZDSE62bo0eoBTW"]
+TRACKS = ["1OcHQQ7A239YbKqKBYw2yw",
+          "2BvI93upqv44QI4hVTvAC3",
+          "53wXk7sMOnvkdAcEpGzu8W"]
+
+PLAYLISTS = [{
+    "user": "Spencer",
+    "user_id": "1229502046",
+    "playlist_id": "2GejolY4BJCsbUxf0yPydq"
+}]
+
+# ----------
+# auth
+# ----------
+
+
+def auth():
+    res = get('https://accounts.spotify.com/authorize?client_id=' + CLIENT_ID + '&response_type=code&redirect_uri=https%3A%2F%2Fnotspotify.me%2Fspotfiycallback')
+    return ret_obj
 
 
 # ----------
@@ -27,7 +49,7 @@ TRACKS = ["1OcHQQ7A239YbKqKBYw2yw", "2BvI93upqv44QI4hVTvAC3", "53wXk7sMOnvkdAcEp
 def get_artist():
     ret_obj = []
     for spotify_id in ARTISTS:
-        response = requests.get('https://api.spotify.com/v1/artists/' + spotify_id)
+        response = get('https://api.spotify.com/v1/artists/' + spotify_id)
         res = response.json()
         obj = {}
         obj['id'] = 42  # TODO: remove
@@ -49,7 +71,7 @@ def get_artist():
 def get_album():
     ret_obj = []
     for spotify_id in ALBUMS:
-        response = requests.get('https://api.spotify.com/v1/albums/' + spotify_id)
+        response = get('https://api.spotify.com/v1/albums/' + spotify_id)
         res = response.json()
         obj = {}
         obj['id'] = 42  # TODO: remove
@@ -80,7 +102,7 @@ def get_album():
 def get_track():
     ret_obj = []
     for spotify_id in TRACKS:
-        response = requests.get('https://api.spotify.com/v1/tracks/' + spotify_id)
+        response = get('https://api.spotify.com/v1/tracks/' + spotify_id)
         res = response.json()
         obj = {}
         obj['id'] = 42  # TODO: remove
@@ -96,3 +118,24 @@ def get_track():
         obj['spotify_uri'] = res['uri']
         ret_obj.append(obj)
     return ret_obj
+
+
+# ------------
+# get_playlist
+# ------------
+
+
+def get_playlist(user):
+    ret = {}
+    pl_info = (next(pl for pl in PLAYLISTS if pl['user'] == user), None)[0]
+    print(pl_info)
+    if pl_info is not None:
+        url = 'https://api.spotify.com/v1/users/' + pl_info['user_id'] + '/playlists/' + pl_info['playlist_id']
+        print(url)
+        res = get(url)
+        ret['followers'] = res['followers']
+        ret['description'] = res['description']
+        ret['images'] = res['images']
+        ret['owner'] = res['owner']
+        ret['tracks'] = res['tracks']
+    return ret
